80cf0072ca955225fe2851751bbe8f9fd35e55f3
daniel-beck <beckdp01@gmail.com>
2025-07-11 22:16:54 -0400

    converted basic scraper to scrape KY SOS info

---
+++ Company.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScraperConsoleApp
+{
+    public class Company
+    {
+        public string? OrganizationNumber { get; set; }
+        public string? Name { get; set; }
+        public string? ProfitOrNonprofit { get; set; }
+        public string? CompanyType { get; set; }
+        public string? Industry { get; set; }
+        public string? NumberOfEmployees { get; set; }
+        public string? PrimaryCounty { get; set; }
+        public string? Status { get; set; }
+        public string? Standing { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? FileDate { get; set; }
+        public string? AuthorityDate { get; set; }
+        public string? LastAnnualReport { get; set; }
+        public string? PrincipalOffice { get; set; }
+        public string? ManagedBy { get; set; }
+        public string? RegisteredAgent { get; set; }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,52 +1,126 @@
-﻿using CsvHelper;
+﻿using System.ComponentModel.DataAnnotations;
+using CsvHelper;
 using CsvHelper.Configuration;
 using HtmlAgilityPack;
 using ScraperConsoleApp;
+// TO DO:
+// 3. Implement outputting to a DOCX file
 
-// the URL of the target Wikipedia page
-string url = "https://en.wikipedia.org/wiki/List_of_Marvel_Cinematic_Universe_films";
+// Get the URL of the target page
 
+string userInput = "";
 var web = new HtmlWeb();
-// downloading to the target page
-// and parsing its HTML content
-var document = web.Load(url);
+HtmlDocument document = new HtmlDocument();
+Company company = new Company();
 
-var nodes = document
-    .DocumentNode
-    .SelectNodes("//*[@id='mw-content-text']/div[1]/table[position()>1 and position()<10]/tbody/tr[position()>1]");
-
-// initializing the list of objects that will
-// store the scraped data
-List<Movie> movies = new List<Movie>();
-
-// looping over the nodes
-// and extract data from them
-foreach (var node in nodes)
+do
 {
-    // add a new Episode instance to
-    // to the list of scraped data
-    movies.Add(new Movie()
+    System.Console.WriteLine("Enter the URL of the Kentucky company profile page. Type 'exit' to quit.");
+    userInput = Console.ReadLine()!;
+
+    if (userInput.ToLower() == "exit")
     {
-        Title = HtmlEntity.DeEntitize(node.SelectSingleNode("th/i/a")?.InnerText),
-        Released = HtmlEntity.DeEntitize(node.SelectSingleNode("td[2]")?.InnerText),
-        Directors = HtmlEntity.DeEntitize(node.SelectSingleNode("td[3]")?.InnerText),
-        Producers = HtmlEntity.DeEntitize(node.SelectSingleNode("td[4]")?.InnerText)
-    });
-}
+        System.Console.WriteLine("Exiting the program.");
+        break;
+    }
 
-CsvConfiguration csvHelper = new CsvConfiguration()
+    try
+    {
+        // downloading to the target page
+        // and parsing its HTML content
+        document = web.Load(userInput);
+    }
+    catch (System.UriFormatException)
+    {
+        System.Console.WriteLine("Invalid URL format. Please try again.");
+        userInput = "";
+    }
+
+} while (userInput == "");
+
+if (userInput != "exit")
 {
-    CultureInfo = System.Globalization.CultureInfo.InvariantCulture,
-    Delimiter = ",",
-    HasHeaderRecord = false
-};
+    var nodes = document.DocumentNode.SelectNodes("//*[@id='MainContent_pInfo']/div/div/div[position() > 0 and position() < 16]");
 
-using (var writer = new StreamWriter("output.csv"))
-using (var csv = new CsvWriter(writer, csvHelper))
-{
-    // writing the records
-    csv.WriteRecords(movies);
+    foreach (var node in nodes)
+    {
+        // Extracting the property name and value
+        var propertyName = HtmlEntity.DeEntitize(node.SelectSingleNode("div[1]")?.InnerText.Trim());
+        var propertyValue = HtmlEntity.DeEntitize(node.SelectSingleNode("div[2]")?.InnerText.Trim());
+
+        switch (propertyName)
+        {
+            case "Organization Number":
+                company.OrganizationNumber = propertyValue;
+                break;
+            case "Name":
+                company.Name = propertyValue;
+                break;
+            case "Profit or Non-Profit":
+                company.ProfitOrNonprofit = propertyValue;
+                break;
+            case "Company Type":
+                company.CompanyType = propertyValue;
+                break;
+            case "Industry":
+                company.Industry = propertyValue;
+                break;
+            case "Number of Employees":
+                company.NumberOfEmployees = propertyValue;
+                break;
+            case "Primary County":
+                company.PrimaryCounty = propertyValue;
+                break;
+            case "Status":
+                company.Status = propertyValue;
+                break;
+            case "Standing":
+                company.Standing = propertyValue;
+                break;
+            case "State":
+                company.State = propertyValue;
+                break;
+            case "File Date":
+                company.FileDate = propertyValue;
+                break;
+            case "Authority Date":
+                company.AuthorityDate = propertyValue;
+                break;
+            case "Last Annual Report":
+                company.LastAnnualReport = propertyValue;
+                break;
+            case "Principal Office":
+                company.PrincipalOffice = propertyValue;
+                break;
+            case "Registered Agent":
+                company.RegisteredAgent = propertyValue;
+                break;
+            case "Country":
+                company.Country = propertyValue;
+                break;
+            case "Managed By":
+                company.ManagedBy = propertyValue;
+                break;
+        }
+    }
+
+    // Print out the company object to the console
+
+    System.Console.WriteLine("Organization Number: " + company.OrganizationNumber);
+    System.Console.WriteLine("Name: " + company.Name);
+    System.Console.WriteLine("Profit or Nonprofit: " + company.ProfitOrNonprofit);
+    System.Console.WriteLine("Company Type: " + company.CompanyType);
+    System.Console.WriteLine("Industry: " + company.Industry);
+    System.Console.WriteLine("Number of Employees: " + company.NumberOfEmployees);
+    System.Console.WriteLine("Primary County: " + company.PrimaryCounty);
+    System.Console.WriteLine("Status: " + company.Status);
+    System.Console.WriteLine("Standing: " + company.Standing);
+    System.Console.WriteLine("State: " + company.State);
+    System.Console.WriteLine("File Date: " + company.FileDate);
+    System.Console.WriteLine("Authority Date: " + company.AuthorityDate);
+    System.Console.WriteLine("Last Annual Report: " + company.LastAnnualReport);
+    System.Console.WriteLine("Principal Office: " + company.PrincipalOffice);
+    System.Console.WriteLine("Registered Agent: " + company.RegisteredAgent);
+    // Add more fields as needed
+
 }
-
-
-       
